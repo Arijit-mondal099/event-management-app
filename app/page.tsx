1b8024f8d2cf7239cb32a4e56e4d138818acb751
Arijit Mondal <arijitm717@gmail.com>
2026-01-11 16:17:20 +0530

    created navbar hero event card

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from "react";
+import ExploreBtn from "@/components/ExploreBtn";
+import events, { EventItem } from "@/lib/constants";
+import EventCard from "@/components/EventCard";
 
-interface HomeProps {
-  
-}
+interface HomeProps {}
 
-const Home: React.FC<HomeProps> = ({  }) => {
+const Home: React.FC<HomeProps> = ({}) => {
   return (
     <section>
-      wellcome to app
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+
+      <ExploreBtn text="Explore Events" />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: EventItem) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
     </section>
   );
 };
